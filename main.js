777463bcd64df4689c165b9c4f63a9a152f5a644
Dario <darionas@gmail.com>
2022-12-20 20:37:47 +0200

    Update index.html_3, main.css_2, main.js_2

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 'Use strict';
-/* navigation */
+
 const modal_nav = document.getElementsByClassName('header_modal_nav')[0];
 const hamburger_icon = document.getElementsByClassName('hamburger_icon')[0];
 const close_icon = document.getElementsByClassName('close_icon')[0];
+let slideIndex = 1;
 
 
+/* Toggle hamburger icon */
 hamburger_icon.onclick = function() {
     modal_nav.style.display = 'block';
 }
@@ -19,3 +21,24 @@ window.onclick = function(event) {
     }
 }
 
+/* FullSize image gallery */
+showSlides(slideIndex);
+
+/* Next/previous controls */
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+/* Thumbnail image controls */
+
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('fullSize_image');
+    if(n > slides.length) {slideIndex = 1}
+    if(n < 1) {slideIndex = slides.length}
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex-1].style.display = 'block';
+}
